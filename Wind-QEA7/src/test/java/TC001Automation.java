f61c41b98384e85d87e5b3fa07f29e424fbeed42
Chowdhury Mobin <chowdhurymobin96@gmail.com>
2022-05-04 19:26:01 -0400

    Chowdhury Mobin

---
+++ Wind-QEA7/src/test/java/TC001Automation.java
@@ -0,0 +1,54 @@
+import java.net.URL;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class TC001Automation {
+
+	WebDriver driver;
+	String URl = "http://invenauto.tech/";
+
+	@BeforeMethod
+	public void setup() {
+		URL driverPath = getClass().getResource("chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", driverPath.getPath());
+		driver = new ChromeDriver();
+		driver.navigate().to(URl);
+		driver.manage().window().maximize();
+	}
+
+	@Test
+	public void LoginWithValidCredentials() throws InterruptedException {
+
+		WebElement signinbtn = driver.findElement(By.xpath("//a[@class='login']"));
+		signinbtn.click();
+		Thread.sleep(1000);
+
+		WebElement username=driver.findElement(By.id("email"));
+		WebElement password=driver.findElement(By.id("passwd"));
+		WebElement login=driver.findElement(By.id("SubmitLogin"));
+
+		username.sendKeys("user123@gmail.com");
+		password.sendKeys("123456");
+		login.click();
+		Thread.sleep(1000);
+
+
+		String actualUrl="http://invenauto.tech/index.php?controller=my-account";
+		String expectedUrl= driver.getCurrentUrl();
+		Assert.assertEquals(expectedUrl,actualUrl);
+	}
+
+	@AfterMethod
+	public void quit() {
+		if (driver != null) {
+			driver.quit();
+		}
+	}
+}
