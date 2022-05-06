d7ef78ead77b2e54f82087ebb5788228d3b52be5
lioneldesroses <lioneldesroses@192.168.1.251>
2022-05-05 20:57:51 -0400

    test 17 update

---
+++ Wind-QEA7/src/test/java/SearchTest.java
@@ -0,0 +1,23 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SearchTest extends SeleniumTestBase{
+
+	@Test
+	public void tc017_cansearchwithspecialkeyword() throws InterruptedException {
+		WebElement search=this.getDriver().findElement(By.id("search_query_top"));
+		WebElement submit=this.getDriver().findElement(By.name("submit_search"));
+
+		search.sendKeys("Dresses");
+		submit.click();
+		Thread.sleep(1000);
+
+		String actualUrl ="http://invenauto.tech/index.php?controller=search&orderby=position&orderway=desc&search_query=Dresses&submit_search=";
+		String expectedUrl= this.getDriver().getCurrentUrl();
+		Assert.assertEquals(expectedUrl,actualUrl);
+	}
+
+	
+}
